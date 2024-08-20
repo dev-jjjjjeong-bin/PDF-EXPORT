244964b80c4f888ec28e858605d2c5cfc9db3780
Correct-Bean <0226pjb@naver.com>
2024-08-20 13:28:46 +0900

    [FEAT] Keep PDF file size the same
    화면 크기 상관없이 PDF 파일 크기 동일하게 유지

--- NAINSIGHT/script.js
+++ NAINSIGHT/script.js
@@ -16,7 +16,6 @@ dropZone.addEventListener('drop', async (event) => {
     dropZone.classList.remove('dragover');
 
     const files = event.dataTransfer.files;
-    // PDF 파일인지 확인
     if (files.length > 0 && files[0].type === 'application/pdf') {
         const file = files[0];
         const fileReader = new FileReader();
@@ -26,23 +25,32 @@ dropZone.addEventListener('drop', async (event) => {
 
             try {
                 const pdf = await pdfjsLib.getDocument(typedArray).promise;
-                pdfContainer.innerHTML = ''; // 기존 PDF 내용을 지움
+                pdfContainer.innerHTML = ''; // 기존 PDF 내용 삭제
+                dropZone.style.opacity = '0'; // 드래그 앤 드랍 존 숨김
+
+                // 드래그 앤 드랍 존 숨긴 후 PDF 표시
+                setTimeout(() => {
+                    dropZone.style.display = 'none';
+                    pdfContainer.style.top = '0'; // PDF를 기존에 드래그 앤 드랍 존이 있던 상단부터 표시
+                }, 500);
 
                 for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
                     const page = await pdf.getPage(pageNum);
-                    const scale = 2.0; // PDF 페이지를 조금 더 크게 렌더링
-                    const viewport = page.getViewport({ scale: scale });
+                    const viewport = page.getViewport({ scale: 1.0 });
+
+                    const scale = pdfContainer.clientWidth / viewport.width;
+                    const scaledViewport = page.getViewport({ scale: scale });
 
                     const canvas = document.createElement('canvas');
                     const canvasContext = canvas.getContext('2d');
-                    canvas.width = viewport.width;
-                    canvas.height = viewport.height;
+                    canvas.width = scaledViewport.width;
+                    canvas.height = scaledViewport.height;
 
                     pdfContainer.appendChild(canvas);
 
                     const renderContext = {
                         canvasContext: canvasContext,
-                        viewport: viewport
+                        viewport: scaledViewport
                     };
                     await page.render(renderContext).promise;
                 }
@@ -58,7 +66,6 @@ dropZone.addEventListener('drop', async (event) => {
     }
 });
 
-// 브라우저 기본 PDF 드래그 앤 드롭 동작 방지
 window.addEventListener('dragover', function(e) {
     e.preventDefault();
 }, false);
